7dfa1ebe8a8952c9bf29cc69263bd5cc6b8a7c1a
Jamith Sheron <jamithsheron5@gmail.com>
2025-06-01 06:47:54 +0530

    create modules as necessary to project and remove unwanted files

--- AMS-ejb/src/main/java/com/jamith/AuctionManagementSystem/ejb/remote/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.jamith.AuctionManagementSystem.ejb.remote;
-
-import com.jamith.AuctionManagementSystem.core.model.User;
-import jakarta.ejb.Remote;
-
-@Remote
-public interface UserService {
-    User getUserById(int id);
-}
