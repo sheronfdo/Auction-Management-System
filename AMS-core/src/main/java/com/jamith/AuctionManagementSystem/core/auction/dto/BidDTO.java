059530bfa9a241376eec1896e82ead9cbd6ede2d
Jamith Sheron <jamithsheron5@gmail.com>
2025-06-03 12:11:52 +0530

    core updated to required as buyer auction level

---
+++ AMS-core/src/main/java/com/jamith/AuctionManagementSystem/core/auction/dto/BidDTO.java
@@ -0,0 +1,22 @@
+package com.jamith.AuctionManagementSystem.core.auction.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BidDTO {
+    private Long bidId;
+    private Long auctionId;
+    private Long buyerId;
+    private String itemName;
+    private BigDecimal bidAmount;
+    private LocalDateTime bidTime;
+}
--- AMS-core/src/main/java/com/jamith/AuctionManagementSystem/core/auction/remote/AuctionManagerRemote.java
+++ AMS-core/src/main/java/com/jamith/AuctionManagementSystem/core/auction/remote/AuctionManagerRemote.java
@@ -2,9 +2,11 @@ package com.jamith.AuctionManagementSystem.core.auction.remote;
 
 import com.jamith.AuctionManagementSystem.core.auction.dto.AuctionDTO;
 import com.jamith.AuctionManagementSystem.core.auction.dto.AuctionSummaryDTO;
+import com.jamith.AuctionManagementSystem.core.auction.dto.BidDTO;
 import com.jamith.AuctionManagementSystem.core.auction.exception.AuctionException;
 import jakarta.ejb.Remote;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Remote
@@ -16,4 +18,6 @@ public interface AuctionManagerRemote {
     void closeAuction(Long auctionId, String sessionToken) throws AuctionException;
     List<AuctionSummaryDTO> listActiveAuctions() throws AuctionException;
     AuctionDTO getAuctionDetails(Long auctionId) throws AuctionException;
+    void placeBid(Long auctionId, BigDecimal bidAmount, String sessionToken) throws AuctionException;
+    List<BidDTO> getBidHistory(String sessionToken) throws AuctionException;
 }
